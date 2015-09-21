e8a18d5dab143e663d5abaae523f6a970fbcb9aa
birdshine <patrickmorganlives@gmail.com>
2015-09-21 23:38:12 +0000

    Update rooms.py
    Added skill interaction function to rooms.

--- rooms.py
+++ rooms.py
@@ -17,7 +17,28 @@ def id_generator(size=7, chars=string.ascii_uppercase + string.digits):
 If there is a better way to do this please get at me.
 This is a master dictionary of all rooms created.
 """
-ROOMS_MASTER_LIST = {}
+ROOMS_MASTER_DICT = {'1234567': 'a premade test room'}
+SKILLS_MASTER_LIST = {
+    'athletics': False,
+    'alchemy': False,
+    'awareness': False,
+    'barter': False,
+    'block': False,
+    'dodge': False,
+    'heal': False,
+    'marksman': False,
+    'melee': False,
+    'operate vehicle': False,
+    'repair': False,
+    'ride': False,
+    'sense motive': False,
+    'sleight of hand': False,
+    'sneak': False,
+    'speech': False,
+    'survival': False,
+    'traps': False,
+    'unarmed': False
+    }
 
 class Room:
     """Represents a game room.
@@ -25,40 +46,31 @@ class Room:
     not defined at initialize.
     """
     
-    def __init__(self, player, alias, desc):
+    def __init__(self, alias, desc):
         """Create room alias and descripion. Id is auto generated.
         Exits and contents will have to be defined with functions.
         """
+        rooms_list = []
         self.id = id_generator()
         self.alias = alias
         self.desc = desc
-        self.player = player
-        self.exits = {
-            'north': None,
-            'north east': None,
-            'north west': None,
-            'east': None,
-            'south east': None,
-            'south': None,
-            'south west': None,
-            'west': None,
-            'in': None,
-            'out': None,
-            'up': None,
-            'down': None,
-        }
-        self.contents = {'player': self.player}
-        ROOMS_MASTER_LIST[self.id] = self.alias
+        """Possible self.exits: Up,down,in,out,se,s,sw,w,nw,n,ne,e."""
+        self.exits = {}
+        self.contents = {'player'}
+        self.skill_interaction = SKILLS_MASTER_LIST
+        ROOMS_MASTER_DICT[self.id] = self.alias
 
     def new_exit(self, direction, alias, locked=False):
         """Creates a new exit within room."""
         self.exits[direction] = [alias,locked]
 
+    def new_skill_interaction(self, skill, dc):
+        """Creates a new skill interaction."""
+        self.skill_interaction[skill] = [True, dc]
+
     def look(self):
         "Player looks about the room."
         print('You are in %s.' % self.alias)
-        print('')
-        print(self.desc)
         print('')
         print('There are exits to the -')
         exit_search = 0    # Set exit_search variable and search for exits.
@@ -70,8 +82,8 @@ class Room:
                 exit_search += 1
         if exit_search == 0:    # There were no additions to exit_search.
             print('There are no exits.')
-            
-"""Debugging tests and commands."""
 
-test_room = Room('player', 'a dusty room', 'This room seems designed for tests.')
-print(ROOMS_MASTER_LIST)
+"""Copyright Patrick Morgan 2015. This program was developed in an effort
+to learn to code the Python language. You may freely use, edit, and
+distribute.
+"""
